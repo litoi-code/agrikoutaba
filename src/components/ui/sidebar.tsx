05e746e9a1a10c75ada10ccefd6e4486279aa93d
mbongmebiang <mbongmebiang@gmail.com>
2026-02-04 07:07:18 +0000

    the sidebar doesn't dage out when i click  an entry on the menu

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -184,7 +184,7 @@ const Sidebar = React.forwardRef<
     const { isMobile, state, openMobile, setOpenMobile } = useSidebar()
 
     if (isMobile === null) {
-      return null;
+      return null
     }
 
     if (collapsible === "none") {
@@ -565,12 +565,20 @@ const SidebarMenuButton = React.forwardRef<
       size = "default",
       tooltip,
       className,
+      onClick,
       ...props
     },
     ref
   ) => {
     const Comp = asChild ? Slot : "button"
-    const { isMobile, state } = useSidebar()
+    const { isMobile, state, setOpenMobile } = useSidebar()
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (isMobile) {
+        setOpenMobile(false)
+      }
+      onClick?.(event)
+    }
 
     const button = (
       <Comp
@@ -579,6 +587,7 @@ const SidebarMenuButton = React.forwardRef<
         data-size={size}
         data-active={isActive}
         className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
+        onClick={handleClick}
         {...props}
       />
     )
